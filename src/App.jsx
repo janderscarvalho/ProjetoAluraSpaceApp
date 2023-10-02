8ec8ff6a3c7c5d2c19ce4c7bd544b5a48598fcd2
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-10-02 13:20:42 -0300

    Inicio de projeto

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import EstilosGlobais from "./components/EstilosGlobais";
+import Cabecalho from "./components/Cabecalho";
+import BarraLateral from "./components/BarraLateral";
+
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(
+    175deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  width: 100%;
+  min-height: 100vh;
+`;
+
+function App() {
+  return (
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+      <BarraLateral />
+      
+    </FundoGradiente>
+  );
+}
+
+export default App;
