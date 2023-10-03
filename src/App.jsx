65d7d12118e07fafd3d5590046649d03e3ed0d2b
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-10-03 11:49:12 -0300

    Criação de galeria e tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import BarraLateral from "./components/BarraLateral";
 import Banner from "./components/Banner";
 import bannerBackground from "./assets/banner.png";
 import Galeria from "./components/Galeria";
+import fotos from './fotos.json'
+import { useState } from "react";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(
@@ -34,7 +36,10 @@ const ConteudoGaleria = styled.section `
   flex-grow: 1;
 `;
 
-function App() {
+const App = () => {
+
+  const[fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -47,7 +52,7 @@ function App() {
             texto="A galeria mais completa de fotos do espaço!"
             backgroundImage={bannerBackground}
           />
-          <Galeria />
+          <Galeria fotos={fotosDaGaleria}/>
 
           </ConteudoGaleria>
         </MainContainer>
