eb03499c75b88cee9840d01a218f6ac43d728cb6
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-10-03 14:48:16 -0300

    Importação da Galeria e Organização de modal

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import bannerBackground from "./assets/banner.png";
 import Galeria from "./components/Galeria";
 import fotos from './fotos.json'
 import { useState } from "react";
+import ModalDeZoom from "./components/ModaldeZoom";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(
@@ -39,7 +40,7 @@ const ConteudoGaleria = styled.section `
 const App = () => {
 
   const[fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
-
+  const[fotoSelecionada, setFotoSelecionada] = useState(null)
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -52,11 +53,14 @@ const App = () => {
             texto="A galeria mais completa de fotos do espaço!"
             backgroundImage={bannerBackground}
           />
-          <Galeria fotos={fotosDaGaleria}/>
+          <Galeria 
+          aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+          fotos={fotosDaGaleria}/>
 
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
+      <ModalDeZoom foto={fotoSelecionada}/>
     </FundoGradiente>
   );
 }
